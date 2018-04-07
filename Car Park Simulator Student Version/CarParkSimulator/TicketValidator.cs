70d49a46fa72fa005cedae7c10be74a6fb2396b3
lucyh18 <37590185+lucyh18@users.noreply.github.com>
2018-04-07 17:44:42 +0100

    Update TicketValidator.cs

--- Car Park Simulator Student Version/CarParkSimulator/TicketValidator.cs
+++ Car Park Simulator Student Version/CarParkSimulator/TicketValidator.cs
@@ -7,5 +7,21 @@ namespace CarParkSimulator
 {
     class TicketValidator
     {
+        private string message;
+       
+        public TicketValidator(string message)
+        {
+            this.message = message;
+        }
+        
+        
+        public string GetMessage(){
+            return message;
+        }
+        
+        public bool CarArrived(){
+        }
+        
+        
     }
 }
