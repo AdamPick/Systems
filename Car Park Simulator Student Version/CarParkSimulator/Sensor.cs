cfd9a843e15fc9b0e771e29adf305bb916a27398
51anusha <37870562+51anusha@users.noreply.github.com>
2018-04-11 16:41:04 +0100

    Update Sensor.cs

--- Car Park Simulator Student Version/CarParkSimulator/Sensor.cs
+++ Car Park Simulator Student Version/CarParkSimulator/Sensor.cs
@@ -5,16 +5,16 @@ using System.Text;
 
 namespace CarParkSimulator
 {       
-    abstract class sensor
+    abstract class sensor                           //abstract because EntrySensor and ExitSensor are inheriting it and override it's methods
     {
-        protected bool carOnSensor;
+        protected bool carOnSensor;                 //protected bool because carOnSensor can only be accessed by classes dervied from sensor
         
-        public sensor() 
+        public sensor()     
         {
             
         }
-        public abstract bool CarDetected();    
-        public abstract bool CarLeftSensor();
+        public abstract bool CarDetected();         //abstract because EntrySensor and ExitSensor inherit it  
+        public abstract bool CarLeftSensor();       //abstract because EntrySensor and ExitSensor inherit it
         
         public bool IsCarOnSensor() 
         {
