8285a784759e7fe6b8c1f3ab36a57d7d17eae057
51anusha <37870562+51anusha@users.noreply.github.com>
2018-04-11 16:40:54 +0100

    Update ExitSensor.cs

--- Car Park Simulator Student Version/CarParkSimulator/ExitSensor.cs
+++ Car Park Simulator Student Version/CarParkSimulator/ExitSensor.cs
@@ -7,18 +7,18 @@ namespace CarParkSimulator
 {
     class ExitSensor: sensor
     {
-        private CarPark carPark;
-        public ExitSensor(CarPark carPark) 
+        private CarPark carPark;                
+        public Exit(CarPark carPark) 
         {
             this.carPark = carPark;
         }     
-        public bool CarDetected()
+        public override bool CarDetected()          //overrides the abstract CarDetected bool already in sensor
         {
             carOnSensor = true;
             carPark.CarArrivedAtExit();           
             return carOnSensor;
         }    
-        public bool CarLeftSensor()
+        public override bool CarLeftSensor()        //overides the abstract CarLeftSensor bool already in sensor
         {
             carPark.carExitedCarPark();
             carOnSensor = false;
