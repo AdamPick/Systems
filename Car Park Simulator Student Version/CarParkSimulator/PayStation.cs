0d7a42311d3da6e07f9ec7a9b9a167c5ec5cc7bf
ANUSHA PATEL <b7015167@hallam.shu.ac.uk>
2018-04-12 11:55:48 +0100

    asdfghjk

--- Car Park Simulator Student Version/CarParkSimulator/PayStation.cs
+++ Car Park Simulator Student Version/CarParkSimulator/PayStation.cs
@@ -2,63 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CarParkSimulator
 {
     class PayStation
     {
-        private Ticket;
-        private ActiveTicket;
-        private string message = "";
-        private bool paid = true;
-
-        public GetTicket(ActiveTicket)
+        private Ticket ticket;
+        public PayStation()
         {
-            this.ticket = new Ticket;
+            this.ticket = new Ticket();
         }
 
-        public string AddTicket()
-        {
-            message = "Please, insert your ticket";
-        }
-
-        public void Ticket()
-        {
-            paid = false;
-        }
-
-        public void PayTicket()
-        {
-            message = "Insert Cash";
-            message = "Insert your payment method";
-            payStation.IsPaid();
-            exitBarrier.Raise();
-        }
-
-        public bool IsPaid()
-        {
-            return paid;
-        }
-
-        private void SetPaid()
+        public int TicketPayment(ActiveTickets activeTickets)
         {
             ticket.SetPaid();
+            return activeTickets.GetTickets();
         }
-
-        public string GetTicket(ActiveTickets)
-        {
-            message = " Thank you, Please collect your ticket. Have a good day!!!";
-        }
-
-        public void ClearMessage()
-        {
-            message = "";
-        }
-
-        public string GetMessage()
-        {
-            return message;
-        }
-
     }
 }
